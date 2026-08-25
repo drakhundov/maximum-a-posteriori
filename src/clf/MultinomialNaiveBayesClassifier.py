5b5de08a8ecd857b390780d5580f3f19a78b2b6c
unknown <abdul.akhundzadeh@gmail.com>
2026-08-25 11:54:47 +0300

    I made a mistake: this is not MAP, this is Naive Bayes Classifier. They both use the same method, but MAP is for feature selection. I keep the code, did some minor refactor, and renamed this repository.

--- src/clf/MultinomialNaiveBayesClassifier.py
+++ src/clf/MultinomialNaiveBayesClassifier.py
@@ -1,31 +1,28 @@
 import numpy as np
 
 
-class DiscreteMaximumAPosteriori:
+class MultinomialNaiveBayesClassifier:
     def __init__(self):
         self.classes = None
-        self.P_y = None
+        self.P_y_log = None
         self.P_xy = None
         self.P_xy_log = None
-        self.P_y_log = None
 
     def fit(self, X_train: np.ndarray, y_train: np.ndarray):
-        if not isinstance(X_train, np.ndarray):
-            X_train = np.array(X_train)
-        if not isinstance(y_train, np.ndarray):
-            y_train = np.array(y_train)
+        X_train = np.asarray(X_train)
+        y_train = np.asarray(y_train)
         if X_train.shape[0] != y_train.shape[0]:
-            raise ValueError("X_train and y_train must have the same size")
-        self.nsamples = X_train.shape[0]
-        self.nfeat = X_train.shape[1]
-        # Assign P(y) for each class.
-        classes, counts = np.unique(y_train, return_counts=True)
-        self.classes = classes
-        self.nclasses = len(classes)
-        self.P_y = counts / y_train.shape[0]
+            raise ValueError("X_train and y_train must have the same number of rows")
+        n_samples, n_features = X_train.shape
+        # counts: array with counts of each class in the training set.
+        self.classes, counts = np.unique(y_train, return_counts=True)
+        nclasses = len(self.classes)
+        # Calculate Log Prior: log(P(y))
+        # self.P_y_log[class_no] = log(P(class))
+        self.P_y_log = np.log(counts / n_samples)
         # Assign P(x | y) for all features.
-        self.P_xy = np.zeros((self.nfeat, self.nclasses))
-        for i, c in enumerate(classes):
+        self.P_xy = np.zeros((n_features, nclasses))
+        for i, c in enumerate(self.classes):
             # Take data points that belong to class 'c'.
             X_c = X_train[y_train == c]
             feat_tot = np.sum(X_c[:, fno])
@@ -36,7 +33,6 @@ class DiscreteMaximumAPosteriori:
                 # Add 0.0001 to avoid log(0.0)
                 self.P_xy[fno][i] = feat_tot / tot
         self.P_xy_log = np.log(self.P_xy)
-        self.P_y_log = np.log(self.P_y)
 
     def predict(self, X_new: np.ndarray):
         log_prob = np.dot(X_new, self.P_xy_log) + self.P_y_log
